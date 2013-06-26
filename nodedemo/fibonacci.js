c0dfc530fa32f02b6cb603c8dc31271cb40c16a6
freele <kremenets0207@gmail.com>
2013-06-26 10:57:16 +0400

    demo

---
+++ nodedemo/fibonacci.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// Fibonacci
+// http://en.wikipedia.org/wiki/Fibonacci_number
+var fibonacci = function(n) {
+	if(n < 1) { return 0;}
+	else if (n<=2) { return 1;}
+	else if (n>2) { return fibonacci(n - 2) + fibonacci(n - 1);}
+};
+
+var fibonacci2 = function(n) {
+	var phi = (1 + Math.sqrt(5))/2;
+	return Math.round((Math.pow(phi, n) - Math.pow(1 - phi, n))/Math.sqrt(5));
+};
+
+var firstkfib = function(k) {
+	var i = 1;
+	var arr = [];
+	for(i = 1; i < k+1; i++){
+		arr.push(fibonacci(i));
+	}
+	return arr;
+};
+
+var firstkfib2 = function(k) {
+        var i = 1;
+        var arr = [];
+        for(i = 1; i < k+1; i++){
+                arr.push(fibonacci2(i));
+        }
+        return arr;
+};
+
+
+
+var fmt = function(arr){ 
+	return arr.join(" ");
+};
+
+var k = 20;
+console.log("firstkfib(" + k + ")");
+console.log(fmt(firstkfib(k)));
+console.log("firstkfib2(" + k + ")");
+console.log(fmt(firstkfib2(k)));
+
